a785e92b941e3f2dec2f2d5dcb63b2759302b311
g-lemoing <glemoing@uoc.edu>
2022-10-16 12:05:34 +0200

    Change States.vue to StatesView.vue

---
+++ src/components/StatesView.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+defineProps({
+  typename:{
+    type: String,
+    required: true,
+  },
+}
+)
+
+const tipus = `${typename}`
+const iconref = `assets/tipus/${typename}.png`
+
+</script>
+
+<template>
+    <img class="icon" src=iconref alt=tipus>
+</template>
+
+<style scoped>
+  .icon{
+    width: 96px;
+    height: 96px;
+  }
+</style>
