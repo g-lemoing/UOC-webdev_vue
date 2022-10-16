f2ae3509b99216dbb6559566b82eed594fb27842
g-lemoing <glemoing@uoc.edu>
2022-10-16 12:09:37 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,33 +2,47 @@
   import { ref, onMounted, computed } from "vue"
   import { services } from "@/hooks/usePokeService.js"
   import { setLocalStorage, getLocalStorage, totalPokemons, pokeIdArray, pokemonsArray } from "@/helpers/pokefunctions.js"
-  import ListView from "@/components/List.vue"
+  import ListView from "@/components/ListView.vue"
 
-  const searchedText = ref("")
-  const arrayPokemons = ref([])
-  const CARDS = 10
+  const searchedText = ref("")            // text to be searched to filter cards
+  const arrayPokemons = ref([])           // Array with result of pokemon Objects
+  // const foundPokemons = ref([])
+  const CARDS = 10                        // Number of card to be displayed on the grid
+  const preservePokemons = false          // Boolean to keep previous set of Pokemons 
 
   const fetchData = async function fetchData(){
     const nrPokemons = await services.getCountPokemons()
-    console.log(nrPokemons)
     const arrayIdPokemons = pokeIdArray(CARDS, nrPokemons)
-    console.log(arrayIdPokemons)
     arrayPokemons.value = await services.getPokemons(arrayIdPokemons)
+    setLocalStorage("arrayIdPokemons", arrayIdPokemons)
+    setLocalStorage("arrayPokemons", arrayPokemons.value)
     console.log(arrayPokemons.value)
   }
 
-const foundPokemons = computed(() => {
-  return arrayPokemons.value.filter( ({name}) => name.toLowerCase().includes(searchedText.value.toLowerCase()))
-})
+const foundPokemons = computed(
+  () => arrayPokemons.value.filter(({pokemon}) => pokemon.name.toLowerCase().includes(searchedText.value.toLowerCase()))
+)
 
-onMounted(() => fetchData())
+onMounted(async() => {
+  const preservePokemons = localStorage.getItem("lockPokemons")
+  if (preservePokemons){
+    // Coming back from detailed card view --> keep previous set of pokemons
+    arrayPokemons.value = getLocalStorage("arrayPokemons")
+  } else{
+    // Request new 10 pokemons set
+    console.log ("Carregant noves dades")
+    fetchData()
+    console.log (foundPokemons.value)
+  }
+  })
 </script>
 
 <template>
   <main>
     <form @submit.prevent="handleSubmit">
       <label for="searchText">Cerqueu un pokemon pel seu nom </label>
-      <input type="text" id = "searchText" name = "searchText" v-model="searchedText">
+      <input type="text" v-model="searchedText">
+      {{searchedText}}
     </form>
     <ListView :pokemons = "arrayPokemons" />
   </main>
